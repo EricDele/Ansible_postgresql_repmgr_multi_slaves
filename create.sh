acfb45c85ac2047063f7b53d2a30e01a934412d8
Eric Deleforterie <eric@deleforterie.com>
2017-11-15 22:34:47 +0100

    Adding the switchover feature, adding more stuff in create steps for repgmr and postgres user, changing sql scripts to return Json result

--- create.sh
+++ create.sh
@@ -4,9 +4,13 @@
 #ansible-playbook -i inventory/inventory.yml --tags "postgresql_create_repmgr" --user=edele postgresql_ha_cascade.yml
 #ansible-playbook -i inventory/inventory.yml --tags "postgresql_configure" --user=edele postgresql_ha_cascade.yml
 #ansible-playbook -i inventory/inventory.yml --tags "postgresql_configure_repmgr_master" --user=edele postgresql_ha_cascade.yml
+#ansible-playbook -i inventory/inventory.yml --tags "postgresql_configure_repmgr_slaves" --user=edele postgresql_ha_cascade.yml
 
 #ALL
 #ansible-playbook -i inventory/inventory.yml --tags "postgresql_create_instances,postgresql_create_repmgr,postgresql_configure,postgresql_configure_repmgr_master,postgresql_configure_repmgr_slaves" --user=edele postgresql_ha_cascade.yml
 
 # Recreate master and slave
-ansible-playbook -i inventory/inventory.yml --tags "postgresql_configure,postgresql_configure_repmgr_master,postgresql_configure_repmgr_slaves" --user=edele postgresql_ha_cascade.yml
+#ansible-playbook -i inventory/inventory.yml --tags "postgresql_configure,postgresql_configure_repmgr_master,postgresql_configure_repmgr_slaves" --user=edele postgresql_ha_cascade.yml
+
+#Switchover to a standby node
+ansible-playbook -i inventory/inventory.yml --tags "postgresql_switchover_to_standby_node" --extra-vars "postgresql_standby_node_for_switchover=vm2" --user=edele postgresql_ha_cascade.yml
